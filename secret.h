3fc5d5d63e99c56003e0fe68db2af3b7d88c13f0
Simon Konglevoll Lønnestad <simon.k.lonnestad@gmail.com>
2019-10-14 23:53:07 +0200

    Create secret.h

---
+++ secret.h
@@ -0,0 +1,5 @@
+#define SECRET_SSID "<ssid>"
+#define SECRET_PASS "<wpa key>"
+#define SECRET_IP <database ip>
+#define SECRET_USERNAME "<database username>"
+#define SECRET_PASSWORD "<database password>"
